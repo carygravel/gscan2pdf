a8b7f27d5ec08a70e994a9a9d230fecbb165836b
Jeffrey Ratcliffe <jffry@posteo.net>
2018-09-12 21:04:35 +0200

    Bumped version to 2.1.5

--- lib/Gscan2pdf/Canvas.pm
+++ lib/Gscan2pdf/Canvas.pm
@@ -18,7 +18,7 @@ BEGIN {
     use Exporter ();
     our ( $VERSION, @EXPORT_OK, %EXPORT_TAGS );
 
-    $VERSION = '2.1.4';
+    $VERSION = '2.1.5';
 
     use base qw(Exporter GooCanvas2::Canvas);
     %EXPORT_TAGS = ();    # eg: TAG => [ qw!name1 name2! ],
--- lib/Gscan2pdf/PageRange.pm
+++ lib/Gscan2pdf/PageRange.pm
@@ -28,7 +28,7 @@ use Glib::Object::Subclass Gtk3::VBox::,
     ),
   ];
 
-our $VERSION = '2.1.4';
+our $VERSION = '2.1.5';
 
 my @widget_list;
 
